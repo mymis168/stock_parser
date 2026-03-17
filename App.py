9c60e8adfc55c07a7ffdf278a9c8dd1b4b46a410
michael <mymis168@gmaile.com>
2026-03-17 14:44:48 +0800

    加入查詢

--- App.py
+++ App.py
@@ -9,10 +9,36 @@ user = "dbeng"
 password = "abc-12345"   
 '''密碼洩漏問題'''
 
+"""
+資料表建立 DDL:
 
 
+CREATE TABLE [dbo].[stocks] (
+    [Id]    INT             IDENTITY (1, 1) NOT NULL,
+    [sid]   CHAR (4)        NOT NULL,
+    [sname] VARBINARY (30)  NOT NULL,
+    [price] DECIMAL (16, 2) NOT NULL,
+    [pdate] DATETIME        CONSTRAINT [DEFAULT_stocks_pdate] DEFAULT GETDATE() NOT NULL,
+    CONSTRAINT [PK_stocks] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
 
+"""
+
+
+# 1. 登入 azure sql server 成功後取得一個 Connection物件
 connect = pymssql.connect(server, user, password, database)
 print("db登入成功")
+
+# 2. 透過該物件(Connection) 產生一個 Cursor物件負責執行 SQL 語法
+cursor = connect.cursor()
+print("cursor 取得成功")
+# 3. 透過 cursor 將 sql 送給 azure sql server 執行
+cursor.execute("select * from stocks")
+
+
+
+
+
+
 
 
